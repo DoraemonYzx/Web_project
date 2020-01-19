2e727d2cc5b7916c1ac6ffbf2e2ca8cff3cb9c1c
DoraemonYzx <1353520146@qq.com>
2020-01-19 10:38:19 +0800

    t

--- demo/run_all_case.py
+++ demo/run_all_case.py
@@ -14,9 +14,9 @@ discover = unittest.defaultTestLoader.discover(test_dir, pattern="test*.py")
 
 if __name__ == '__main__':
     report_dir = './test_report'
-    now = time.strftime("%Y-%m-%d %H_%M_%S")
-    report_name = report_dir+'/'+now+'result.html'
-
+    # now = time.strftime("%Y-%m-%d %H_%M_%S")
+    # report_name = report_dir+'/'+now+'result.html'
+    report_name = report_dir+'/'+'result.html'
     with open(report_name, 'wb') as f:
         runner = HTMLTestRunner(stream=f, title="Demo测试报告", description="！！！！")
         runner.run(discover)
